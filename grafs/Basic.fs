b99691a653703ddc831f649084ff6b71ae405455
Nathan Sanders <ncsander@indiana.edu>
2011-07-12 22:40:58 -0700

    Refactor DFS algorithm to put dfsLoop back inside dfsWith

--- grafs/Basic.fs
+++ grafs/Basic.fs
@@ -64,26 +64,26 @@ let bfs (g : Graph<'k,unit>) (start : 'k) : Graph<'k,Bfs<'k>> =
     loop (queue @ vs)
   loop [start]
 
-let dfsWith (g : Graph<'k,unit>) action : Graph<'k,Dfs<'k>> =
+let dfsCore (g : Graph<'k,unit>) (orderBy : Graph<'k,Dfs<'k>> -> seq<KeyValuePair<'k,Vertex<'k,_>>>) action : Graph<'k,Dfs<'k>> =
   let g' = annotate g {colour=White; discover=0; finish=0; parent=None; cConnected= -1}
   let time = ref 0
-  let rec dfsVisit (u : 'k) k =
-    let value = g'.[u]
+  let mutable cConnected = 0
+  let rec dfsLoop (g : Graph<'k,Dfs<'k>>) (root : 'k) k =
+    let value = g.[root]
     time := !time + 1
     value.meta <- {value.meta with colour=Grey; discover=time.Value; cConnected=k}
-    for v in value.edges |> Seq.filter (fun v -> g'.[v].meta.colour = White) do
-      g'.[v].meta <- { g'.[v].meta with parent=Some u }
-      dfsVisit v k
+    for v in value.edges |> Seq.filter (fun v -> g.[v].meta.colour = White) do
+      g.[v].meta <- { g.[v].meta with parent=Some root }
+      dfsLoop g v k
     time := !time + 1
     value.meta <- {value.meta with colour=Black; finish=time.Value}
-    action u
-  let mutable cConnected = 0
-  for pair in g' do 
+    action root
+  for pair in orderBy g' do 
     if pair.Value.meta.colour = White then 
-      dfsVisit pair.Key cConnected
+      dfsLoop g' pair.Key cConnected
       cConnected <- cConnected + 1
   g'
-let dfs (g : Graph<'k,unit>) : Graph<'k,Dfs<'k>> = dfsWith g ignore
+let dfs (g : Graph<'k,unit>) : Graph<'k,Dfs<'k>> = dfsCore g seq ignore
 
 let cons x l = x :: l
 let update (r : ref<'a>) f = r.Value <- f r.Value
@@ -94,28 +94,14 @@ module State =
     state.Value
 
 let topologicalSort (g : Graph<'k,unit>) =
-  State.run (fun t -> dfsWith g (cons >> update t) |> ignore) []
+  State.run (fun t -> dfsCore g seq (cons >> update t) |> ignore) []
 
 let stronglyConnectedComponents (g : Graph<'k,unit>) : Set<Set<'k>> =
   let forwardG = dfs g
-  let g' = annotate (reverse g) {colour=White; discover=0; finish=0; parent=None; cConnected= -1}
-  let time = ref 0
-  let rec dfsVisit (u : 'k) k =
-    let value = g'.[u]
-    time := !time + 1
-    value.meta <- {value.meta with colour=Grey; discover=time.Value; cConnected=k}
-    for v in value.edges |> Seq.filter (fun v -> g'.[v].meta.colour = White) do
-      g'.[v].meta <- { g'.[v].meta with parent=Some u }
-      dfsVisit v k
-    time := !time + 1
-    value.meta <- {value.meta with colour=Black; finish=time.Value}
-  let mutable cConnected = 0
-  for pair in g' |> Seq.sortBy (fun pair -> -forwardG.[pair.Key].meta.finish) do 
-    if pair.Value.meta.colour = White then 
-      dfsVisit pair.Key cConnected
-      cConnected <- cConnected + 1
-  set (g' |> Seq.groupBy (fun pair -> pair.Value.meta.cConnected) 
-          |> Seq.map (snd >> Seq.map (fun pair -> pair.Key) >> set))
+  dfsCore (reverse g) (Seq.sortBy (fun pair -> -forwardG.[pair.Key].meta.finish)) ignore
+  |> Seq.groupBy (fun pair -> pair.Value.meta.cConnected)
+  |> Seq.map (snd >> Seq.map (fun pair -> pair.Key) >> set)
+  |> set
 
 
 printfn "This program is not intended for running. I created the wrong type of project."
